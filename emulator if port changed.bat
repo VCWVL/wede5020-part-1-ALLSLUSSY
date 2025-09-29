3af3958be94a15937b6be593aea8b5de21134e86
ALLSLUSSY <allenzungu973@gmail.com>
2025-09-29 15:53:56 +0200

    Delete emulator if port changed.bat

--- emulator if port changed.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-set /P port=Enter your port:
-START "runas /user:administrator" cmd /K "cd C:\Users\lab_services_student\AppData\Local\Android\Sdk\platform-tools & adb connect localhost:%port%"
